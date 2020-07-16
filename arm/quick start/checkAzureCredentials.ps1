682fd2139e2369ef35b916047597112f54c69305
madsamuel <stgeorgi@microsoft.com>
2020-07-16 10:24:04 -0700

    update and cleanup

---
+++ arm/quick start/checkAzureCredentials.ps1
@@ -0,0 +1,14 @@
+param(
+	[string] [Parameter(Mandatory=$true)] $username,
+	[string] [Parameter(Mandatory=$true)] $password
+)
+
+powershell.exe
+
+Install-Module AzureAD
+Import-Module AzureAD
+
+$ErrorActionPreference = 'Stop'
+
+$Credential = New-Object System.Management.Automation.PsCredential($username, (ConvertTo-SecureString $password -AsPlainText -Force))
+Connect-AzureAD -AzureEnvironmentName 'AzureCloud' -Credential $Credential
