d73ba0128bbdcc74fbbf8bd820dcaa48e04d6059
madsamuel <stgeorgi@microsoft.com>
2020-07-17 10:29:56 -0700

    more test on output

--- arm/quick start/checkAzureCredentials.ps1
+++ arm/quick start/checkAzureCredentials.ps1
@@ -3,33 +3,39 @@ param(
 	[string] [Parameter(Mandatory=$true)] $password
 )
 
-#region body
-Write-Output "Enter"
+#region string literals
+$failure = "Provided account is missing Owner role." 
+$success = "Provided account has Owner role." 
+#endregion 
 
+#region body
 $ErrorActionPreference = 'Stop'
 
-#region find role
-foreach ($x in $assignment) { 
-    if ($x.RoleDefinitionName -eq "Owner") 
-        { $found = 1; break} 
-    Else
-        {$found = 0} }
+    #region authenticate
+    $password = ConvertTo-SecureString $password -AsPlainText -Force
+    $pscredential = New-Object System.Management.Automation.PSCredential($username, $password)
+    #endregion
 
+    #region connetc and test roles
+    try {
+        Connect-AzAccount -Credential $pscredential
+
+        $assignment = Get-AzRoleAssignment -SignInName $username
+
+        foreach ($x in $assignment) { 
+            if ($x.RoleDefinitionName -eq "Owner") 
+                { $found = $success; break} 
+            Else
+            {$found = $failure } 
+        }
+    }
+    catch { 
+        $found = "Provided account is missing Owner role."
+    }
+    #endregion
+
+    #region output
     $DeploymentScriptOutputs = @{}
     $DeploymentScriptOutputs['text'] = $found
+    #endregion
 #endregion
-
-
-
-#region test creds
-#Write-Output "Creds"
-
-#$Credential = New-Object System.Management.Automation.PsCredential($username, (ConvertTo-SecureString $password -AsPlainText -Force))
-#Connect-AzureAD -AzureEnvironmentName 'AzureCloud' -Credential $Credential
-#endregion
-
-#region assignment
-#Get-AzUserAssignedIdentity -ResourceGroupName $ResourceGroupName -Name "$username"
-#endregion
-
-#endregion 
