fdb5c50aa8793b41a4015826cb8787c6be0a7387
madsamuel <stgeorgi@microsoft.com>
2020-07-17 15:52:21 -0700

    Update offline-script-used-to-test-code.ps1

--- arm/aadcredvalidation/offline-script-used-to-test-code.ps1
+++ arm/aadcredvalidation/offline-script-used-to-test-code.ps1
@@ -3,6 +3,8 @@ $username = "admin@gt1101.onmicrosoft.com"
 # @param
 $password = ""
 
+Import-Module AzureAD
+
 #region string literals
 $failure = "Auth to Azure AD failed." 
 $success = "Auth to Azure AD completed. " 
@@ -18,28 +20,29 @@ $ErrorActionPreference = 'Stop'
 
     #region authenticate
     # do not need this as I am alread runnign in the context of an admin 
-    # $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
-    # $pscredential = New-Object System.Management.Automation.PSCredential($username, $securePassword)
+     $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+     $pscredential = New-Object System.Management.Automation.PSCredential($username, $securePassword)
     #endregion
 
     #region connetc and test roles
     try {
-        Connect-AzureAD    
+        Connect-AzureAD -Credential $pscredential 
         $found = $success
+        #region password reset
+        try {
+            Update-AzureADSignedInUserPassword -CurrentPassword (ConvertTo-SecureString $password -AsPlainText -Force ) -NewPassword (ConvertTo-SecureString $password -AsPlainText -Force)
+            $found = $found + " Password is correct."
+        }
+        catch {
+            $found = $found + " Password is incorrect."
+        }
+        #endregion
     }
     catch { 
         $found = $failure
     }  
     #endregion
-    #region passwor reset
-    try {
-        Update-AzureADSignedInUserPassword -CurrentPassword (ConvertTo-SecureString $password -AsPlainText -Force ) -NewPassword (ConvertTo-SecureString $password -AsPlainText -Force)
-        $found = $found + " Password is correct."
-    }
-    catch {
-        $found = $found + " Password is incorrect."
-    }
-
+    
     $DeploymentScriptOutputs['text'] = $found
   
 #endregion
