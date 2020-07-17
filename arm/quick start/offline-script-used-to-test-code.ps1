9ade2682439d2d14635d27ecd7a38ef3cd5f9874
madsamuel <stgeorgi@microsoft.com>
2020-07-17 11:24:04 -0700

    Updated

---
+++ arm/quick start/offline-script-used-to-test-code.ps1
@@ -0,0 +1,42 @@
+﻿# @param
+$username = "admin@gt1101.onmicrosoft.com"
+# @param
+$password = "ReverseParol44"
+
+#region string literals
+$failure = "Provided account is missing Owner role." 
+$success = "Provided account has Owner role." 
+#endreginon 
+
+#region body
+Write-Output "Enter"
+
+$ErrorActionPreference = 'Stop'
+
+#region find role
+$password = ConvertTo-SecureString $password -AsPlainText -Force
+$pscredential = New-Object System.Management.Automation.PSCredential($username, $password)
+
+try {
+    Connect-AzAccount -Credential $pscredential
+
+    $assignment = Get-AzRoleAssignment -SignInName $username
+
+    foreach ($x in $assignment) { 
+        if ($x.RoleDefinitionName -eq "Owner") { 
+            $found = $success; 
+            break
+        } 
+        Else {
+            $found = $failure 
+        } 
+    }
+}
+catch { 
+    $found = "Provided account is missing Owner role."
+}
+
+$DeploymentScriptOutputs = @{}
+$DeploymentScriptOutputs['text'] = $found
+
+#endregion
