82e5464a0de6ba155066563d2e7cfe1ad374305b
madsamuel <madsamuel@gmail.com>
2021-04-22 22:41:42 -0700

    synching

--- manage azure sub.ps1
+++
@@ -1,8 +0,0 @@
-Connect-AzAccount
-
-# CSE for creating users
-$vmss = "advm"
-$rg = "GT090717a"
-$extensionName = "customscript"
-Remove-AzVMExtension -ResourceGroupName $rg -VMName $vmss -Name $extensionName 
-Get-AzVMExtension -ResourceGroupName $rg -VMName $vmss -Name $extensionName 
